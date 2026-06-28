38fd81ec97d2540dbc6ebe8057a470f33a0f1637
samikshya11-web <samidhakal1212@gmail.com>
2026-06-28 07:27:39 +0545

    data type

---
+++ C_Data_Types/b_value_type_boolean/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace b_value_type_boolean;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Boolean data type
+        bool isMale = true;
+
+        if (isMale == true)
+        {
+            Console.WriteLine("Suman is male.");
+        }
+    }
+}
