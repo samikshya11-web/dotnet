033339649976f0f9d712be892f53b69190876c61
samikshya11-web <samidhakal1212@gmail.com>
2026-07-22 16:06:55 +0545

    Untrack all bin and obj folders

---
+++ F_Exception_handling/a_exception/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace a_exception;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int dividend = 12, divisor =0, quotient;
+        try
+        {
+            quotient = dividend/divisor;
+            Console.WriteLine("The result =" + quotient);
+            
+        }
+        catch( Exception e)
+        {
+            Console.WriteLine("Exception detected:" + e);
+        } 
+    }
+}
---
+++ F_Exception_handling/b_exception/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace b_exception;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[5];
+        try
+        {
+           nums[10] = 90;
+           Console.WriteLine(nums[10]); 
+        }
+        catch(IndexOutOfRangeException e)
+        {
+            Console.WriteLine("Index out of bounds:" + e);
+        }
+        
+    }
+}
