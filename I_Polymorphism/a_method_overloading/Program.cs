654f534609fdcfadcd13dd76f506a25826d10d82
samikshya11-web <samidhakal1212@gmail.com>
2026-07-24 22:03:08 +0545

    Add new project folders

---
+++ I_Polymorphism/a_method_overloading/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace a_method_overloading;
+#nullable disable
+class MyNum
+{
+    public int add(int a, int b)
+    {
+        return (a + b);
+    }
+    public int add(int a, int b, int c)
+    {
+        return (a + b + c);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyNum obj = new MyNum();
+        Console.WriteLine("The sum = " + obj.add(10, 20));
+        Console.WriteLine("The sum = " + obj.add(10, 20, 40));
+    }
+}
